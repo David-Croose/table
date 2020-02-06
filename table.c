f00021afbbb7ec629fc4cadb80519d22e4e4bcfc
David-Croose <178449962@qq.com>
2020-02-06 17:09:12 +0800

    it can work now

--- table.c
+++ table.c
@@ -3,17 +3,40 @@
 #include <stdarg.h>
 #include "table.h"
 
+static int32_t strcat_safe2(char *p1, uint32_t p1len, const char *p2) {
+    uint32_t i;
+    uint32_t p2len;
+
+    if (!p1 || !p1len || !p2) {
+        return -1;
+    }
+    if ((p2len = strlen(p2)) == 0) {
+        return 0;
+    }
+
+    for (i = 0; i < p1len - 1 && p1[i]; i++);
+    if (i >= p1len - 1) {
+        return -2;
+    }
+
+    if (p2len > p1len - 1 - i) {
+        return -3;
+    }
+    memcpy(&p1[i], p2, p2len);
+    return p2len;
+}
+
 /**
  * append the source string to destination string, every append with a NULL terminated.
  * e.g:
  *      char buf[10] = {0};
  *      strcat_safe(buf, sizeof(buf), "_123");   // buf="_123", 0
  *      strcat_safe(buf, sizeof(buf), "_456");   // buf="_123", 0, "_456", 0
- * @param: p1. the destination string
- * @param: p1len. the length(in bytes) of @p1
- * @param: p2. the source string
- * @return: <0 : failure
- *          >=0: the appended bytes
+ * @param p1: the destination string
+ * @param p1len: the length(in bytes) of @p1
+ * @param p2: the source string
+ * @return: <0  : failure
+ *          >=0 : the appended bytes
  */
 static int32_t strcat_safe(char *p1, uint32_t p1len, const char *p2) {
     uint32_t i, j;
@@ -53,8 +76,12 @@ static int32_t addstring(table_handle_t *h, uint32_t col, const char *buf) {
 }
 
 /**
- * get a string from the specify string
- *
+ * get a string(e.g: {'1', '2', '3', 0, '4', '5', '6', 0, '7', '8', '9', 0}) by
+ * specified index
+ * @param h: a @table instance
+ * @param index: the sequence number of the string(start with 0) in @h
+ * @return: !NULL : the address of the string
+ *          NULL  : the string can not be found
  */
 static char *getitem(table_handle_t *h, uint32_t index) {
     uint32_t i;
@@ -65,29 +92,36 @@ static char *getitem(table_handle_t *h, uint32_t index) {
     }
 
     if (index == 0) {
-        return h->pool;
+        return (h->pool[0] ? &h->pool[0] : NULL);
     }
-    if (index > h->poollen / 2) {
+    if (index > h->poollen / 2) {   // if index > max index
         return NULL;
     }
 
     for (find = 0, i = 1; i < h->poollen - 1; i++) {
-        if (h->pool[i] == 0) {
-            if (++find == index) {
-                return &h->pool[i + 1];
-            }
+        if (h->pool[i] == 0 && ++find == index) {
+            return (h->pool[i + 1] ? &h->pool[i + 1] : NULL);
         }
     }
     return NULL;
 }
 
+/**
+ * append a string to a buffer, if the string is shorter than a specify value, padding it
+ * @param p: the buffer to be appended to
+ * @param plen: the length(in bytes) of @p
+ * @param item: the string append to @p
+ * @param itemlen: the length(in bytes) of @item
+ * @return: <0  : failure
+ *          >=0 : the wrote bytes
+ */
 static int32_t addstring_pad(char *p, uint32_t plen, const char *item, uint32_t itemlen) {
     uint32_t i;
-    char buf[CONFIG_MAXITEMLEN];
+    char buf[CONFIG_ITEMSIZE];
     uint32_t space;
     uint32_t _itemlen;
 
-    if (!p || !plen || !item || !itemlen) {
+    if (!p || !plen || !item || (itemlen < MINIMAL_ITEMSIZE || itemlen > CONFIG_ITEMSIZE)) {
         return -1;
     }
 
@@ -98,27 +132,36 @@ static int32_t addstring_pad(char *p, uint32_t plen, const char *item, uint32_t 
     }
 
     memset(buf, ' ', sizeof(buf));
-    space = itemlen - 1;
+    space = itemlen - 1;                // the last character can not be filled, it must be ' ' or others
     _itemlen = strlen(item);
     if (_itemlen <= space) {
-        memcpy(&p[i], item, _itemlen);
+        memcpy(buf, item, _itemlen);
     } else {
-        memcpy(&p[i], item, space);
-        p[space - 1] = '.';
-        p[space - 2] = '.';
-        p[space - 3] = '.';
+        memcpy(buf, item, space);
+        buf[space - 1] = '.';
+        buf[space - 2] = '.';
+        buf[space - 3] = '.';
     }
 
-    return 0;
+    if (itemlen > plen - 1 - i) {
+        return -3;
+    }
+    memcpy(&p[i], buf, itemlen);
+    return itemlen;
 }
 
-int32_t table_init(table_handle_t *h, uint8_t *mem, uint32_t memlen, int32_t totcol, const char *end) {
-    if (!h || !mem || !memlen || !totcol || !end) {
+int32_t table_init(table_handle_t *h, char *mem, uint32_t memlen, int32_t totcol, const char *end) {
+    uint32_t i;
+
+    if (!h || !mem || !memlen || (!totcol || totcol > CONFIG_MAXCOLLEN) || !end) {
         return -1;
     }
 
     memset(mem, 0, memlen);
     memset(h, 0, sizeof(*h));
+    for (i = 0; i < totcol; i++) {
+        h->colwidth[i] = MINIMAL_ITEMSIZE;
+    }
     h->pool = (char *)mem;
     h->poollen = memlen;
     h->totcol = totcol;
@@ -152,7 +195,7 @@ int32_t table_add_seprow(table_handle_t *h, char sep) {
 }
 
 int32_t table_write(table_handle_t *h, const char *fmt, ...) {
-    static char buf[CONFIG_MAXITEMLEN];
+    static char buf[CONFIG_ITEMSIZE];
     va_list args;
 
     memset(buf, 0, sizeof(buf));
@@ -175,7 +218,6 @@ int32_t table_read(table_handle_t *h, char *mem, uint32_t memlen, uint32_t *real
     char *p;
     uint32_t i;
     uint32_t col;
-    char end[3];
     int32_t writelen;
 
     if (!h || !mem || !memlen || !reallen) {
@@ -184,7 +226,6 @@ int32_t table_read(table_handle_t *h, char *mem, uint32_t memlen, uint32_t *real
 
     memset(mem, 0, memlen);
     *reallen = 0;
-    memcpy(end, h->end, sizeof(end));
 
     for (i = 0, col = 0; (p = getitem(h, i)) != NULL; i++) {
         if ((writelen = addstring_pad(mem, memlen, p, h->colwidth[col])) < 0) {
@@ -193,7 +234,7 @@ int32_t table_read(table_handle_t *h, char *mem, uint32_t memlen, uint32_t *real
         *reallen += writelen;
 
         if (++col >= h->totcol) {
-            if ((writelen = strcat_safe(mem, memlen, end)) < 0) {
+            if ((writelen = strcat_safe2(mem, memlen, h->end)) < 0) {
                 return -3;
             }
             *reallen += writelen;
@@ -214,17 +255,58 @@ void table_destroy(table_handle_t *h) {
 }
 
 //===========================================================================================
+#define FAIL_IF(x)      if ((x) != 0) { goto error; }
+
+char *str[] = {
+    "123",
+    "4567",
+    "89",
+    "ABCDEFG",
+    "_abcd",
+    "__xxx",
+    "xyz0",
+    "~789cdef",
+    "!gg",
+    "hjklopq",
+    "22334",
+    "zxc_89",
+};
+
+/// int main() {
+///     char buf[20] = {0};
+///     int i;
+/// 
+///     for (i = 0; i < sizeof(str) / sizeof(str[0]); i++) {
+///         addstring_pad(buf, sizeof(buf), str[i]);
+///     }
+/// 
+/// 
+///     return 0;
+/// }
+
 int main() {
-    char buf[20] = {0};
+    table_handle_t h;
+    static char buf[100];
+    static char readbuf[sizeof(buf) * 3];
+    uint32_t reallen;
     int i;
-    int res;
 
-    for (i = 0; i < 20; i++) {
-        res = strcat_safe(buf, sizeof(buf), "_");
-        printf("buf=%s, res=%d\n", buf, res);
+    FAIL_IF(table_init(&h, buf, sizeof(buf), 3, "\n"));
+    for (i = 0; i < sizeof(str) / sizeof(str[0]); i++) {
+        if (table_write(&h, str[i])) {
+            break;
+        }
     }
+    FAIL_IF(table_read(&h, readbuf, sizeof(readbuf), &reallen));
+    printf("%s\n", readbuf);
+    table_destroy(&h);
 
+    printf("ok\n");
     return 0;
+
+error:
+    printf("error\n");
+    return -1;
 }
 //===========================================================================================
 
--- table.h
+++ table.h
@@ -1,7 +1,13 @@
 #include <stdint.h>
 
 #define CONFIG_MAXCOLLEN          (10)      // max colume length(in bytes)
-#define CONFIG_MAXITEMLEN         (40)      // max item length(in bytes)
+#define CONFIG_ITEMSIZE           (8)       // max item length(in bytes)
+
+#define MINIMAL_ITEMSIZE          (5)
+
+#if (CONFIG_ITEMSIZE < MINIMAL_ITEMSIZE)
+#error 'CONFIG_ITEMSIZE' is wrong
+#endif
 
 typedef struct {
     char *pool;
@@ -14,7 +20,7 @@ typedef struct {
     char end[3];                            // "\n" or "\r" or "\r\n"
 } table_handle_t;
 
-int32_t table_init(table_handle_t *h, uint8_t *mem, uint32_t memlen, int32_t totcol, const char *end);
+int32_t table_init(table_handle_t *h, char *mem, uint32_t memlen, int32_t totcol, const char *end);
 int32_t table_add_seprow(table_handle_t *h, char sep);
 int32_t table_write(table_handle_t *h, const char *fmt, ...);
 int32_t table_read(table_handle_t *h, char *mem, uint32_t memlen, uint32_t *reallen);
